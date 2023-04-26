42653d7204626d95aef3d20caaf6669d6f446b29
Nitish Kumar <myhobbyissolvemath@gmail.com>
2023-04-27 04:44:21 +0530

    updated

--- src/components/ProductManage/ProductManage.jsx
+++ src/components/ProductManage/ProductManage.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import AddProduct from './AddProduct/AddProduct'
 import EditProduct from './EditProduct/EditProduct'
-import { Col, Container, Row } from 'react-bootstrap'
+import { Col, Row } from 'react-bootstrap'
 
 export default function ProductManage() {
     return (
         <div>
             <h1 className="text-center my-2 border border-success border-5 bg-secondary bg-gradient">
-                Add Product Page
+                Product Management System
             </h1>
             <Row className="justify-content-center m-0 text-center">
                 <Col className='ps-5' md={4}>
---
+++ src/components/Profile/Orders/Order/OrderItem/OrderItem.jsx
@@ -0,0 +1,74 @@
+import axios from 'axios'
+import React, { useContext } from 'react'
+import { Button, Col, Row } from 'react-bootstrap'
+import base_url from '../../../../../api/bootapi'
+import AuthContext from '../../../../Context/AuthProvider'
+
+export default function OrderItem({ orderItem, orderId, count }) {
+
+    const {user, setOrders} = useContext(AuthContext) 
+
+    const product = orderItem.product
+    const image = "data:image/png;base64," + product.image
+
+    const cancelOrderItemHandler = () => {
+        if(count > 1) {
+            axios.delete(`${base_url}/order/${user.id}/cancelOrderItem/${orderItem.id}`)
+                .then(
+                    (response) => {
+                        setOrders(response?.data)
+                        alert("Order item deleted successfully")
+                    },
+                    (error) => {
+                        alert(error + "something went wrong")
+                    }
+                )
+        }
+        else {
+            axios.delete(`${base_url}/order/${user.id}/cancelOrder/${orderId}`)
+            .then(
+                (response) => {
+                    setOrders(response?.data)
+                    alert("cancel")
+                },
+                (error) => {
+                    alert(error + "Something went wrong")
+                }
+            )
+        }
+    }
+
+    return (
+        <>
+            <Row>
+                <Col md={2}>
+                    <img src={image} alt="" style={{ width: "100px", height: "100px" }} />
+                </Col>
+                <Col md={8}>
+                    <p className='text-start text-truncate'>
+                        <small>{product.description}</small>
+                    </p>
+                    <Row>
+                        <Col>
+                            <p className='text-start'>
+                                <b>Quantity: </b>
+                                <span>{
+                                    orderItem.quantity
+                                }</span>
+                            </p>
+                        </Col>
+                    </Row>
+                </Col>
+            </Row>
+            <Row>
+                <Col>
+                    <Button className='btn-sm' variant="primary">Order Again</Button>
+                </Col>
+                <Col>
+                    <Button className='btn-sm' onClick={cancelOrderItemHandler} variant='danger'>Cancel Item</Button>
+                </Col>
+            </Row> 
+            <hr />
+        </>
+    )
+}
